41327bcd328fc9b7c47e5a438a8b761532105213
sjk1170 <129317039+sjk1170@users.noreply.github.com>
2023-08-02 10:56:54 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function setup() {
     currentPage: 'home', // 'home', 'service'
     currentLanguage: 'english',
     dialNumber1: '1002201@umb.com',
-    dialNumber2: '1002201@umb.com',
+    dialNumber2: '8605890@umb.com',
     dialNumber3: 'scott.kern@umb.com',
     services: [],
 
